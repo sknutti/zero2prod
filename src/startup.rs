0045bba5c4b18c9c43331d65ca23d97bdc7296fa
Scott Knutti <scott.knutti@liveviewtech.com>
2021-07-15 14:33:23 -0600

    Finished chapter 3

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,17 @@
 use std::net::TcpListener;
 
-use actix_web::{App, HttpServer, dev::Server, web};
+use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgPool;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
     // handles all transport level concerns
-    let server = HttpServer::new(|| {
+    let server = HttpServer::new(move || {
         // handles all application logic -- routing, middleware, request handlers, etc.
         App::new()
+            .app_data(db_pool.clone())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
     })
